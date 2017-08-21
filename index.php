239f862de64b3b9ec043bb932825adad833b95e7
adarsh anchan <anchanadarsh@gmail.com>
2017-08-21 12:47:52 +0530

    upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <title>Webpage Title</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <?php include '_partial/head.php' ?>
+</head>
+
+<body>
+    <!--
+	<header>
+		<?php include '_partial/header.php' ?>
+	</header>
+-->
+    <div class="clearfix"></div>
+    <section id="section-home1" class="container-fluid p-lr-none p-tb-md">
+        <div class="container">
+            <div class="atmosphere-outer">
+                <div class="particle-set-square square_0">
+                    <div class="c-particle red-particle tl"></div>
+                    <div class="c-particle red-particle tr"></div>
+                    <div class="c-particle red-particle bl"></div>
+                    <div class="c-particle red-particle br"></div>
+                </div>
+                <div class="particle-set-square square_10">
+                    <div class="c-particle green-particle tl"></div>
+                    <div class="c-particle green-particle tr"></div>
+                    <div class="c-particle green-particle bl"></div>
+                    <div class="c-particle green-particle br"></div>
+                </div>
+                <div class="particle-set-square square_20">
+                    <div class="c-particle yellow-particle tl"></div>
+                    <div class="c-particle yellow-particle tr"></div>
+                    <div class="c-particle yellow-particle bl"></div>
+                    <div class="c-particle yellow-particle br"></div>
+                </div>
+                <div class="particle-set-square square_30">
+                    <div class="c-particle blue-particle tl"></div>
+                    <div class="c-particle blue-particle tr"></div>
+                    <div class="c-particle blue-particle bl"></div>
+                    <div class="c-particle blue-particle br"></div>
+                </div>
+                <div class="particle-set-square square_40">
+                    <div class="c-particle red-particle tl"></div>
+                    <div class="c-particle red-particle tr"></div>
+                    <div class="c-particle red-particle bl"></div>
+                    <div class="c-particle red-particle br"></div>
+                </div>
+                <div class="particle-set-square square_50">
+                    <div class="c-particle green-particle tl"></div>
+                    <div class="c-particle green-particle tr"></div>
+                    <div class="c-particle green-particle bl"></div>
+                    <div class="c-particle green-particle br"></div>
+                </div>
+                <div class="particle-set-square square_60">
+                    <div class="c-particle yellow-particle tl"></div>
+                    <div class="c-particle yellow-particle tr"></div>
+                    <div class="c-particle yellow-particle bl"></div>
+                    <div class="c-particle yellow-particle br"></div>
+                </div>
+                <div class="particle-set-square square_70">
+                    <div class="c-particle blue-particle tl"></div>
+                    <div class="c-particle blue-particle tr"></div>
+                    <div class="c-particle blue-particle bl"></div>
+                    <div class="c-particle blue-particle br"></div>
+                </div>
+                <div class="particle-set-square square_80">
+                    <div class="c-particle purple-particle tl"></div>
+                    <div class="c-particle purple-particle tr"></div>
+                    <div class="c-particle purple-particle bl"></div>
+                    <div class="c-particle purple-particle br"></div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    <!--
+	<footer>
+		<?php include '_partial/footer.php' ?>
+	</footer>	
+-->
+    <?php include '_partial/footer-scripts.php' ?>
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// All custom codes with comment
+
+$(".c-particle").click(function(){
+    $(".c-particle").removeClass("make_particle_big");
+    $(this).addClass("make_particle_big");
+});
